43daed9a565a5a15b5a3530f8d5f4ed63bc22bb4
TylerAddams <hoangliverstar@gmail.com>
2024-05-25 08:41:29 +0700

    first commit

---
+++ Lab5_Mail/Menu.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Lab5_Mail
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBai1 = new System.Windows.Forms.Button();
+            this.btnBai2 = new System.Windows.Forms.Button();
+            this.btnBai3 = new System.Windows.Forms.Button();
+            this.btnBai4 = new System.Windows.Forms.Button();
+            this.btnBai5 = new System.Windows.Forms.Button();
+            this.btnBai6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(260, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Menu - Sending & Receiving Email in C#";
+            // 
+            // btnBai1
+            // 
+            this.btnBai1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai1.Location = new System.Drawing.Point(84, 105);
+            this.btnBai1.Name = "btnBai1";
+            this.btnBai1.Size = new System.Drawing.Size(315, 48);
+            this.btnBai1.TabIndex = 1;
+            this.btnBai1.Text = "Bài 1: Viết ứng dụng cho phép gửi mail.";
+            this.btnBai1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai1.UseVisualStyleBackColor = true;
+            this.btnBai1.Click += new System.EventHandler(this.btnBai1_Click);
+            // 
+            // btnBai2
+            // 
+            this.btnBai2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai2.Location = new System.Drawing.Point(84, 175);
+            this.btnBai2.Name = "btnBai2";
+            this.btnBai2.Size = new System.Drawing.Size(315, 48);
+            this.btnBai2.TabIndex = 2;
+            this.btnBai2.Text = "Bài 2: Viết ứng dụng cho phép đọc mail (IMAP).";
+            this.btnBai2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai2.UseVisualStyleBackColor = true;
+            this.btnBai2.Click += new System.EventHandler(this.btnBai2_Click);
+            // 
+            // btnBai3
+            // 
+            this.btnBai3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai3.Location = new System.Drawing.Point(84, 250);
+            this.btnBai3.Name = "btnBai3";
+            this.btnBai3.Size = new System.Drawing.Size(315, 48);
+            this.btnBai3.TabIndex = 3;
+            this.btnBai3.Text = "Bài 3: Viết ứng dụng cho phép đọc mail (POP). ";
+            this.btnBai3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai3.UseVisualStyleBackColor = true;
+            this.btnBai3.Click += new System.EventHandler(this.btnBai3_Click);
+            // 
+            // btnBai4
+            // 
+            this.btnBai4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai4.Location = new System.Drawing.Point(457, 105);
+            this.btnBai4.Name = "btnBai4";
+            this.btnBai4.Size = new System.Drawing.Size(315, 48);
+            this.btnBai4.TabIndex = 4;
+            this.btnBai4.Text = "Bài 4: Quản lý phòng vé";
+            this.btnBai4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai4.UseVisualStyleBackColor = true;
+            this.btnBai4.Click += new System.EventHandler(this.btnBai4_Click);
+            // 
+            // btnBai5
+            // 
+            this.btnBai5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai5.Location = new System.Drawing.Point(457, 175);
+            this.btnBai5.Name = "btnBai5";
+            this.btnBai5.Size = new System.Drawing.Size(315, 48);
+            this.btnBai5.TabIndex = 5;
+            this.btnBai5.Text = "Bài 5: Hôm nay ăn gì?";
+            this.btnBai5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai5.UseVisualStyleBackColor = true;
+            this.btnBai5.Click += new System.EventHandler(this.btnBai5_Click);
+            // 
+            // btnBai6
+            // 
+            this.btnBai6.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBai6.Location = new System.Drawing.Point(457, 250);
+            this.btnBai6.Name = "btnBai6";
+            this.btnBai6.Size = new System.Drawing.Size(315, 48);
+            this.btnBai6.TabIndex = 6;
+            this.btnBai6.Text = "Bài 6: Viết ứng dụng Email Client.";
+            this.btnBai6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBai6.UseVisualStyleBackColor = true;
+            this.btnBai6.Click += new System.EventHandler(this.btnBai6_Click);
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 473);
+            this.Controls.Add(this.btnBai6);
+            this.Controls.Add(this.btnBai5);
+            this.Controls.Add(this.btnBai4);
+            this.Controls.Add(this.btnBai3);
+            this.Controls.Add(this.btnBai2);
+            this.Controls.Add(this.btnBai1);
+            this.Controls.Add(this.label1);
+            this.Name = "Menu";
+            this.Text = "Lab5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBai1;
+        private System.Windows.Forms.Button btnBai2;
+        private System.Windows.Forms.Button btnBai3;
+        private System.Windows.Forms.Button btnBai4;
+        private System.Windows.Forms.Button btnBai5;
+        private System.Windows.Forms.Button btnBai6;
+    }
+}
+
